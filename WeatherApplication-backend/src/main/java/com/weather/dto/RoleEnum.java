ca59bd170776ed235ba473f037701131507103bf
sayed <abu.sayed@spectrum-bd.com>
2024-06-07 22:23:48 +0600

    Created README detail.

--- WeatherApplication-backend/src/main/java/com/weather/dto/RoleEnum.java
+++ WeatherApplication-backend/src/main/java/com/weather/dto/RoleEnum.java
@@ -3,5 +3,10 @@ package com.weather.dto;
 public enum RoleEnum {
     ROLE_USER,
     ROLE_MODERATOR,
-    ROLE_ADMIN
+    ROLE_ADMIN,
+    ROLE_READ,
+    ROLE_WRITE,
+    ROLE_UPDATE,
+    ROLE_DELETE,
+    ROLE_REPORT
 }
