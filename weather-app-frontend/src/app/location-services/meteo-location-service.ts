2a063df0e6da1f27048c556c20545a4c1b23975b
sayed <abu.sayed@spectrum-bd.com>
2024-06-08 09:05:33 +0600

    Web APIs edit

--- weather-app-frontend/src/app/location-services/meteo-location-service.ts
+++ weather-app-frontend/src/app/location-services/meteo-location-service.ts
@@ -25,34 +25,34 @@ export class MeteoLocationService extends BaseService {
 
     public fetchLocations(payload: string): Observable<any> {
         return this.http.get(
-            baseURL + '/geo/location?name=' + payload, httpOptions
+            baseURL + '/v1/geo/location?name=' + payload, httpOptions
         );
     }
 
     public saveFavouriteLocation(payload: Object): Observable<any> {
         return this.http.post(
-            baseURL + '/favourite/location', payload, httpOptions
+            baseURL + '/v1/favourite/location', payload, httpOptions
         );
     }
 
     public fecthFavouriteLocation(map: Map<string, any>): Observable<any> {
         let params = this.mapToHttpParams(map);
         return this.http.get(
-            baseURL + '/favourite/location'+params, httpOptions
+            baseURL + '/v1/favourite/location'+params, httpOptions
         );
     }
 
     public fecthPagedFavouriteLocations(map: Map<string, any>): Observable<any> {
         let params = this.mapToHttpParams(map);
         return this.http.get(
-            baseURL + '/favourite/locations'+params, httpOptions
+            baseURL + '/v1/favourite/locations'+params, httpOptions
         );
     }
 
     public fetchDailyWeatherForecastInfo(map: Map<string, any>): Observable<any>{
         let params = this.mapToHttpParams(map);
         return this.http.get(
-                baseURL + '/geo/daily-weather-forecast'+params, httpOptions
+                baseURL + '/v1/geo/daily-weather-forecast'+params, httpOptions
             );
     }
 
@@ -60,7 +60,7 @@ export class MeteoLocationService extends BaseService {
     public fetchHourlyWeatherForecastInfo(map: Map<string, any>): Observable<any>{
         let params = this.mapToHttpParams(map);
         return this.http.get(
-                baseURL + '/geo/hourly-weather-forecast'+params, httpOptions
+                baseURL + '/v1/geo/hourly-weather-forecast'+params, httpOptions
             );
     }
 
